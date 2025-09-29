d10485e29378ff05f09055a2a87c8497b4aa7621
bsmeele <bas.smeele2000@gmail.com>
2025-09-29 17:04:37 +0200

    initial testing setup

---
+++ tests/tests.cpp
@@ -0,0 +1,31 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "nes.h"
+
+TEST_CASE( "RAM read/write", "[RAM]" ) {
+  NES nes = NES();
+  nes.bus.write(0x0001, 1);
+  nes.bus.write(0x0802, 2);
+  nes.bus.write(0x1003, 3);
+  nes.bus.write(0x1804, 5);
+
+  REQUIRE(  nes.bus.read(0x0001) == 1 );
+  REQUIRE(  nes.bus.read(0x0002) == 2 );
+  REQUIRE(  nes.bus.read(0x0003) == 3 );
+  REQUIRE(  nes.bus.read(0x0004) == 5 );
+  
+  REQUIRE(  nes.bus.read(0x0801) == 1 );
+  REQUIRE(  nes.bus.read(0x0802) == 2 );
+  REQUIRE(  nes.bus.read(0x0803) == 3 );
+  REQUIRE(  nes.bus.read(0x0804) == 5 );
+
+  REQUIRE(  nes.bus.read(0x1001) == 1 );
+  REQUIRE(  nes.bus.read(0x1002) == 2 );
+  REQUIRE(  nes.bus.read(0x1003) == 3 );
+  REQUIRE(  nes.bus.read(0x1004) == 5 );
+
+  REQUIRE(  nes.bus.read(0x1801) == 1 );
+  REQUIRE(  nes.bus.read(0x1802) == 2 );
+  REQUIRE(  nes.bus.read(0x1803) == 3 );
+  REQUIRE(  nes.bus.read(0x1804) == 5 );
+}
